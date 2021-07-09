b080a81a146ef27d74efedf842736e55149f338b
CodingWithLaura <laura.ganss@web.de>
2021-07-09 15:40:54 +0200

    Generate sinus waves which can be manipulated in frequenzy and amplitude

---
+++ Main.hs
@@ -0,0 +1,47 @@
+import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Builder as B
+import Data.Foldable
+import System.Process
+import Text.Printf
+
+type Pulse = Float
+type Seconds = Float
+type Samples = Float
+type Hz = Float
+type Semitones = Float
+
+outputFilePath :: FilePath
+outputFilePath = "output.bin"
+
+volume :: Float
+volume = 0.5
+
+sampleRate :: Samples
+sampleRate = 48000.0
+
+pitchStandard :: Hz
+pitchStandard = 440.0
+
+f :: Semitones -> Hz
+f n = pitchStandard * (2 ** (1.0 / 12.0)) ** n 
+
+freq :: Hz -> Seconds -> [Pulse]
+freq hz duration =
+  map (* volume) $ map sin $ map (* step) [0.0 .. sampleRate * duration]
+  where
+    step = (hz * 2 * pi) / sampleRate
+
+wave :: [Pulse]
+wave = concat [freq (pitchStandard + i * 100.0) duration | i <- [0 .. 10]]
+  where
+    duration = 1.0
+
+save :: FilePath -> IO ()
+save filePath = B.writeFile filePath $ B.toLazyByteString $ fold $ map B.floatLE wave
+
+play :: IO ()
+play = do
+  save outputFilePath
+  _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate outputFilePath
+  return ()
+
